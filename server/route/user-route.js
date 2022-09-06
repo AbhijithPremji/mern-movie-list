217f74dff3962fd392c56a423a2111230247e229
badushaebrahim <bbadusha13@gmail.com>
2022-09-06 21:20:01 +0530

    full user registration and user login comlete

--- server/route/user-route.js
+++ server/route/user-route.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db/dbconnect')
 const User = require('../models/User')
-
-
+const bcrypt = require('bcrypt')
+const {
+	createJWT,
+     } = require('../utils/jwt')
+     const jwt = require('jsonwebtoken');
 router.get('/test',(req,res)=>{
 	res.status(200).json({message:"working"})
 	
@@ -11,19 +14,50 @@ router.get('/test',(req,res)=>{
 })
 
 router.post('/register',(req,res)=>{
-	var user = new User(req.body);
-	user.save()
-		.then((user) => {
-			res.json({message:"user made"});
-		}).catch(err=>{
-			res.status(400).json({message:"user not made"+err}).send()
-		})
+	let { name,dob, email, password, } = req.body;
+	console.log(name,dob,email,password)
+	User.findOne({email: email})
+	.then(user=>{
+	   if(user){
+	      return res.status(422).json({ errors: [{ user: "email already exists" }] });
+	   }else {
+	      const user = new User({
+		name: name,
+		dob:dob,
+		email: email,
+		password: password,
+	      });
+	      bcrypt.genSalt(10, function(err, salt) { bcrypt.hash(password, salt, function(err, hash) {
+	      if (err) throw err;
+	      user.password = hash;
+	      user.save()
+		  .then(response => {
+		     res.status(200).json({
+		       success: true,
+		       result: response
+		     })
+		  })
+		  .catch(err => {
+		    res.status(500).json({
+		       errors: [{ error: err }]
+		    });
+		 });
+	      });
+	   });
+	  }
+       }).catch(err =>{
+	   res.status(500).json({
+	     errors: [{ error: 'Something went wrong'+err }]
+	   });
+       })
 });
+
 
 router.post('/login',(req,res) =>{
 	let { email, password } = req.body;
 	User.findOne({ email: email }).then(user => {
 	  if (!user) {
+		console.log("not found57")
 	    return res.status(404).json({
 	      errors: [{ user: "not found" }],
 	    });
@@ -42,23 +76,28 @@ router.post('/login',(req,res) =>{
 	  jwt.verify(access_token, process.env.TOKEN_SECRET, (err,
    decoded) => {
 	    if (err) {
-	       res.status(500).json({ erros: err });
+	       res.status(500).json({ erros2: err });
 	    }
 	    if (decoded) {
 		return res.status(200).json({
 		   success: true,
 		   token: access_token,
-		   message: user
+		   message: user.email
 		});
 	      }
 	    });
 	   }).catch(err => {
-	     res.status(500).json({ erros: err });
+	     res.status(500).json({ erros1: err });
 	   });
 	 }
       }).catch(err => {
 	 res.status(500).json({ erros: err });
       });
 });
+
+function hash(password){
+	
+}
+
 
 module.exports = router
