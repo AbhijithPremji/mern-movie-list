2123a7fad2a701603693a240002309cbbdf3cbe7
badushaebrahim <bbadusha13@gmail.com>
2022-09-06 22:28:29 +0530

    token study in progress

--- server/route/user-route.js
+++ server/route/user-route.js
@@ -126,7 +126,92 @@ decoded) => {
 });
 
 
+router.put('/userupdate',(req,res)=>{
+	let { name,dob, email,id } = req.body;
+	 let errors = [];
+	 if (!name) {
+		errors.push({ name: "required" });
+	      }
+	      if (!email) {
+		errors.push({ email: "required" });
+	      }
+	      if (!emailRegexp.test(email)) {
+		errors.push({ email: "invalid" });
+	      }
+	     
+	      if (!dob) {
+		errors.push({ dob: "required" });
+	      }
+	      
+	    
+	      if (errors.length > 0) {
+		return res.status(422).json({ errors: errors });
+	      }
+	      User.findOne({email: email})
+	.then(user=>{
+	   if(user){
+	      return res.status(422).json({ errors: [{ user: "email already exists" }] });
+	   }else {
+	// //      const user = new User({
+	       
+	       User.updateOne({_id:id},{
+		name: name,
+		dob:dob,
+		email: email,
+	       }).then(responce=>{
+		res.status(200).json({
+			success: true,
+			result: response
+		})
+	       }).catch(err => {
+		res.status(500).json({
+		   errors: [{ error: err }]
+		});
+
+    
+	  });
+	 }
+      }).catch(err =>{
+	  res.status(500).json({
+	    errors: [{ error: 'Something went wrong' }]
+	  });
+      })
+});
 
 
 
+router.put('/tok',(req,res) =>{
+	const token =
+	req.body.token || req.query.token || req.headers["x-access-token"];
+	// console.log(token)
+	const t = parseJwt(token)
+	// console.log(t)
+      if (!token) {
+	return res.status(403).send("A token is required for authentication");
+      }
+      try {
+	// const decoded = jwt.verify(token, process.env.TOKEN_KEY);
+	// req.user = decoded;
+	// console.log(typeof(decoded))
+	jwt.verify(access_token, process.env.TOKEN_SECRET, (err,
+		decoded) => {
+			 if (err) {
+			//     res.status(500).json({ erros: err });
+			console.log(200,":", err)
+			 }
+			 if (decoded) {
+			     return res.status(200).json({
+				success: true,
+				token: access_token,
+				message: user._id,
+			     });
+			   }
+			 });
+      } catch (err) {
+	return res.status(401).send("Invalid Token");
+      }
+})
+function parseJwt (token) {
+	return JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString());
+    }
 module.exports = router
